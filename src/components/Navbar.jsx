199c43ae3fc0c78ccea84426cc8c8fcd60658f1f
muzammil-gitdev <muzammil.uiit@gmail.com>
2025-11-26 18:08:52 +0500

    final

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -60,7 +60,7 @@ const Navbar = ({ onBookClick }) => {
             <div className="max-w-screen-xl flex flex-wrap items-center justify-between mx-auto p-4">
                 <a href="#" onClick={(e) => scrollToSection(e, '#home')} className="flex items-center space-x-3 rtl:space-x-reverse group">
                     <span className="self-center text-2xl font-bold whitespace-nowrap text-gray-900 group-hover:text-blue-600 transition-colors">
-                        Drive<span className="text-blue-600 group-hover:text-gray-900 transition-colors">X</span>
+                        Drive<span className="text-blue-600 group-hover:text-gray-900 transition-colors">XsMax</span>
                     </span>
                 </a>
                 <div className="flex md:order-2 space-x-3 md:space-x-0 rtl:space-x-reverse">
